d17f655d232fd61f005d40938b7fdf6b4b20864a
SamulVlad <55761105+SamulVlad@users.noreply.github.com>
2024-04-30 10:26:10 +0300

    Add files via upload

---
+++ src/edu/training/jc/simple_output/StairsOutput.java
@@ -0,0 +1,10 @@
+package edu.training.jc.simple_output;
+
+public class StairsOutput {
+
+	public static void main(String[] args) {
+System.out.println(" \"To wisely live your life, you don't need to know much\n\t Just remember two main rules for the  beginning:\n\t\t You better starve, than eat whatever\n\t And better be alone, than with whoever.\"\n Omar Khayyam,  Rubaiyat");		// TODO Auto-generated method stub
+
+	}
+
+}
